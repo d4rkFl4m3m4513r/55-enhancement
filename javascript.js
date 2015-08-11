431bfe3ebf226ae8d7721852a54daa57b5d9c8a5
darkflamemaster <tempsvictorsabino6@gmail.com>
2015-08-11 06:12:58 -0300

    connected json

--- javascript.js
+++ javascript.js
@@ -1,6 +1,30 @@
+data = 0;
 $("body").append('<div id="arrow" onclick="moveup()">   </div>');
 
 function moveup(){
 	$("html, body").animate({
 	scrollTop: 0
 }, 400);
+}
+
+function getLink(){
+	currentUrl = window.location.href;
+	modifiedUrl = currentUrl.split('html');
+
+	return modifiedUrl[0] + "json" + modifiedUrl[1];
+}
+
+
+	currentUrl = getLink();
+
+	console.log(currentUrl);
+	$.ajax({
+		type: 'GET',
+		dataType: "json",
+		url: '3.json',
+		success: function(data){
+				$.each(data.threads ,function(){
+						console.log("teste");
+			});
+		}
+	});
